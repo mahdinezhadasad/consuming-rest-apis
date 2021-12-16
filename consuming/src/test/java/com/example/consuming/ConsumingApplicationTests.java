2274e4b700b4a2da1eb779dcdb599768fd5a12d6
mahdinezhadasad <mahdinezhadasad@gmail.com>
2021-12-16 01:02:12 +0100

    Integration Testing on Consuming API

--- consuming/src/test/java/com/example/consuming/ConsumingApplicationTests.java
+++ consuming/src/test/java/com/example/consuming/ConsumingApplicationTests.java
@@ -2,12 +2,39 @@ package com.example.consuming;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class ConsumingApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testDownloadPicture(){
+
+		ResponseEntity<byte[]>  response = this.restTemplate.getForEntity("https://http.cat/200",byte[].class);
+
+		assertThat(response.getStatusCode(),equalTo(HttpStatus.OK));
+
+
+	}
+
 
 	@Test
 	public void contextLoads() {
